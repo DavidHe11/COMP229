abae99b7e8aeb5312b44a36ca24ce953c7f2f8ef
David-Home <Davidyhe1194@gmail.com>
2016-09-24 14:55:47 -0400

    Initial Commit

---
+++ Assignment1-ResumeSite/Assignment1-ResumeSite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Assignment1_ResumeSite.Global" Language="C#" %>
---
+++ Assignment1-ResumeSite/Assignment1-ResumeSite/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Assignment1_ResumeSite.Startup))]
+namespace Assignment1_ResumeSite
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
